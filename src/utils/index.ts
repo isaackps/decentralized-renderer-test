3006aca3323a1ecaf9e0be736557f5206272da27
Ng Han Inn <43451336+nghaninn@users.noreply.github.com>
2025-05-22 18:14:07 +0800

    fix: add types (#17)
    Co-authored-by: nghaninn <Ng Han Inn 43451336+nghaninn@users.noreply.github.com>

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,18 @@
 import {
   isRawV3Document,
   OpenAttestationDocument,
+  RawVerifiableCredential,
   SignedVerifiableCredential,
   vc,
 } from "@trustvc/trustvc";
 
 /**
  * This function is necessary to extract the document data from the data. It makes decentralized renderer templates compatible with v2, v3 and w3c vc document version.
- * @param {OpenAttestationDocument | SignedVerifiableCredential} document - The document itself.
+ * @param {OpenAttestationDocument | SignedVerifiableCredential | RawVerifiableCredential} document - The document itself.
  * @returns {document} The extracted data from the document.
  */
 export const getDocumentData = (
-  document: OpenAttestationDocument | SignedVerifiableCredential
+  document: OpenAttestationDocument | SignedVerifiableCredential | RawVerifiableCredential
 ): any => {
   if (isRawV3Document(document) || vc.isSignedDocument(document) || vc.isRawDocument(document)) {
     return document.credentialSubject;
